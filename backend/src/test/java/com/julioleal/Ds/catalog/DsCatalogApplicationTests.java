67c991353bfb99703c0e03b91af90d9671f4acc8
Julio Cesar Freitas Leal <julio.leal@alunos.ifsuldeminas.edu.br>
2022-07-02 12:32:14 -0300

    Back end project created

---
+++ backend/src/test/java/com/julioleal/Ds/catalog/DsCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.julioleal.Ds.catalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
